70e0f56924cab1ab22a8cf893d7ef10ff89fa44e
AlexanderKotinov <alexander777ck@gmail.com>
2018-09-02 23:01:00 +0300

    Added sidenav vs toolbox.

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -8,17 +8,25 @@ import {FormControl, FormGroup} from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
   signUpForm: FormGroup;
-
+  maxDate: Date;
   constructor() { }
 
   ngOnInit() {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 1);
     this._signUpFromInit();
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm.value);
   }
 
   private _signUpFromInit() {
     this.signUpForm = new FormGroup({
-      email: new FormControl(),
-      password: new FormControl()
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      birthday: new FormControl(null, Validators.required),
+      agree: new FormControl(null, Validators.required)
     });
   }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule } from '@angular/material';
+import {
+  MatButtonModule, MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatNativeDateModule, MatSidenavModule, MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
@@ -8,13 +15,23 @@ import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule } fr
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   declarations: []
 })
