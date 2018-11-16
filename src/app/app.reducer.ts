2d4e97b0b9d3810f78fb1c56b4fe61b1722189cd
AlexanderKotinov <alexander777ck@gmail.com>
2018-11-16 23:24:59 +0200

    Added ui reducer.

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,22 +1,14 @@
+uiimport * as fromUi from './shared/ui.reducer';
+import {ActionReducerMap, createFeatureSelector, createSelector} from "@ngrx/store";
+import {createFeatureReducerFactory} from "@ngrx/store/src/utils";
+
 export interface State {
-  isLoading: boolean;
+  ui: fromUi.State;
 }
 
-const initialState: State = {
-  isLoading: false
+export const reducers: ActionReducerMap<State> = {
+  ui: fromUi.uiReducer
 };
 
-export function appReducer(state = initialState, action) {
-  switch (action.type) {
-    case 'START_LOADING':
-      return {
-        isLoading: true
-      };
-    case 'STOP_LOADING':
-      return {
-        isLoading: false
-      };
-    default:
-      return state;
-  }
-}
+export const getUiState = createFeatureSelector<fromUi.State>('ui');
+export const getIsLoadin = createSelector(getUiState, fromUi.getIsLoading);
