2f73d96000c091ce48739a68a2d47c54a998ed8a
AlexanderKotinov <alexander777ck@gmail.com>
2018-11-21 17:43:21 +0200

    Added reducer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,10 @@ import { HeaderComponent } from './components/navigation/header/header.component
 import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
 import { StopTrainingDialogComponent } from './components/training/current-training/stop-training-dialog/stop-training-dialog.component';
 import { environment } from '../environments/environment';
-import { AuthModule } from './auth.module';
+import { AuthModule } from './components/auth/auth.module';
 import {SharedModule} from './shared/shared.module';
 import {AngularFireAuthModule} from 'angularfire2/auth';
-import {AuthRoutingModule} from './auth-routing.module';
+import {AuthRoutingModule} from './components/auth/auth-routing.module';
 import { reducers } from './app.reducer';
 import { StoreModule } from '@ngrx/store';
 
--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { SignupComponent } from './components/auth/signup/signup.component';
-import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { SharedModule } from './shared/shared.module';
-import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from '../../shared/shared.module';
+import { AppRoutingModule } from '../../app-routing.module';
 
 @NgModule({
   imports: [
